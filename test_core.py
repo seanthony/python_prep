baa29eb269157b5b5919e6bc2a35fe7279e16466
seanthony <sean@basecampcodingacademy.org>
2018-07-20 13:34:04 -0500

    added test_add_hours to test_core, up next will be to implement add_hours in core

--- test_core.py
+++ test_core.py
@@ -5,3 +5,11 @@ def test_create_user_dictionary():
     user_dictionary = create_user_dictionary(
         ['Sean, 0\n', 'Nate, 0\n', 'Evil Nate, 0\n'])
     assert user_dictionary == {'Sean': 0, 'Nate': 0, 'Evil Nate': 0}
+
+
+def test_add_hours():
+    user_dictionary = {'Sean': 0, 'Nate': 0, 'Evil Nate': 0}
+    username = 'Sean'
+    hours = 10
+    add_hours(user_dictionary, username, hours)
+    assert user_dictionary == {'Sean': 10, 'Nate': 0, 'Evil Nate': 0}
