64897a2428c8c7146956dbbac594efc31e39d89c
seanthony <sean@basecampcodingacademy.org>
2018-07-20 13:12:51 -0500

    fixed error in test case for create_user_dictionary in test_core.py (missing comma)

--- test_core.py
+++ test_core.py
@@ -3,5 +3,5 @@ from core import *
 
 def test_create_user_dictionary():
     user_dictionary = create_user_dictionary(
-        ['Sean, 0\n', 'Nate, 0\n', 'Evil Nate 0\n'])
+        ['Sean, 0\n', 'Nate, 0\n', 'Evil Nate, 0\n'])
     assert user_dictionary == {'Sean': 0, 'Nate': 0, 'Evil Nate': 0}
