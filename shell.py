85ea4c6f73b27daf2a088f7d160ae7a6ed0e331b
seanthony <sean@basecampcodingacademy.org>
2018-07-20 13:36:34 -0500

    added add_hours from core to shell

--- shell.py
+++ shell.py
@@ -17,10 +17,23 @@ def get_username(user_dictionary):
             print(username, 'is an invalid option.')
 
 
+def get_hours():
+    ''' None -> int
+    returns a valid number of hours given by the user
+    '''
+    while True:
+        hours = input('How many hours? ')
+        if hours.isdigit():
+            return int(hours)
+        print('invalid entry!')
+
+
 def main():
     roster_raw_info = disk.open_file('./roster.txt')
     user_dictionary = core.create_user_dictionary(roster_raw_info)
     username = get_username(user_dictionary)
+    hours = get_hours()
+    core.add_hours(user_dictionary, username, hours)
 
 
 if __name__ == '__main__':
