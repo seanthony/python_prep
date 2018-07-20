8c9f1afc8c1eddfd8e418a59402e9ec3901c4d3c
seanthony <sean@basecampcodingacademy.org>
2018-07-20 11:44:44 -0500

    added docstring to open_file in disk

--- disk.py
+++ disk.py
@@ -1,4 +1,9 @@
 def open_file(filename):
+    ''' str -> [str]
+    opens a file of filename and returns a list
+    of the line data, but skips the first line
+    '''
+
     with open(filename, 'r') as file:
         file.readline()
         file_information = file.readlines()
