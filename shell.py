15eed8aec9a9dc2dab092dda32a22af40f471683
seanthony <sean@basecampcodingacademy.org>
2018-07-20 14:13:31 -0500

    added a user option menu and moved the get_most_hours and add_hours from core to separate functions

--- shell.py
+++ shell.py
@@ -2,6 +2,19 @@ import disk
 import core
 
 
+def get_option():
+    '''() -> str
+    returns the option for the users choice
+    '''
+    print('What would you like to do today?', '\n\t1. Add Hours',
+          '\n\t2. Find Most Hours', '\n\t3. Exit')
+    while True:
+        choice = input('>>> ')
+        if choice in ['1', '2', '3']:
+            return choice
+        print('oops!', choice, 'is invalid')
+
+
 def get_username(user_dictionary):
     ''' {str: int} -> str 
     prints out the valid usernames
@@ -28,13 +41,34 @@ def get_hours():
         print('invalid entry!')
 
 
+def add_user_hours(user_dictionary):
+    username = get_username(user_dictionary)
+    hours = get_hours()
+    core.add_hours(user_dictionary, username, hours)
+    print(hours, 'added to', username)
+
+
+def find_most_hours(user_dictionary):
+    usernames = core.get_most_hours(user_dictionary)
+    for user in usernames:
+        print(user, 'has worked', user_dictionary.get(user), 'hours')
+
+
 def main():
     filename = './roster.txt'
     roster_raw_info = disk.open_file(filename)
     user_dictionary = core.create_user_dictionary(roster_raw_info)
-    username = get_username(user_dictionary)
-    hours = get_hours()
-    core.add_hours(user_dictionary, username, hours)
+
+    while True:
+        print()
+        choice = get_option()
+        if choice == '1':
+            add_user_hours(user_dictionary)
+        elif choice == '2':
+            find_most_hours(user_dictionary)
+        elif choice == '3':
+            break
+
     file_string = core.create_file_string(user_dictionary)
     disk.write_file(filename, file_string)
 
